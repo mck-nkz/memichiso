1e41aeb7d3a198b18f81e2db356e6e0bad1e7642
mck <mck.nkz@gmail.com>
2015-10-11 12:27:16 +0900

    header背景画像変更

--- chan/app/webroot/theme/m-single_kyan/Elements/header.php
+++ chan/app/webroot/theme/m-single_kyan/Elements/header.php
@@ -2,15 +2,15 @@
 <!--
 <h1><?php //$this->BcBaser->logo() ?></h1>
 -->
-<div>
-<div style="color:#999;">幸福度100%<br />
+<div class="header-wrap">
+<div>幸福度100%<br />
 祭る文化で生きている</div>
 
 <h1><?php $this->BcBaser->logo() ?></h1>
 
-<div style="color:#999;">（活動拠点は前道荘(メーミチソウ)(仮)です）</div>
+<div>（活動拠点は前道荘(メーミチソウ)(仮)です）</div>
 
-<div style="margin:0;padding:0 0 20px 0;color:#999;">むすまいのしま 南風原町字<ruby>喜屋武<rt>ちゃん</rt></ruby></div>
+<div style="margin:0;padding:0 0 20px 0;">むすまいのしま 南風原町字<ruby>喜屋武<rt>ちゃん</rt></ruby></div>
 
 
 <img src="<?php $this->BcBaser->themeUrl() ?>img/kyan_map.png" alt="" />
