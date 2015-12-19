fd938b55caa24da16b3a5c68214b488a5128c1de
mck <mck.nkz@gmail.com>
2015-12-19 15:30:45 +0900

    カレンダーベージのレイアウト修正,bootstrap適用,GAトラッキングコード追加

--- calendar/index.php
+++ calendar/index.php
@@ -1,14 +1,52 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-<meta charset="UTF-8">
-<title>前道荘 宿泊予約カレンダー</title>
+    <meta charset="utf-8">
+    <title>前道荘 宿泊予約カレンダー</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/base.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 <body>
-<div>
-<img src="images/memichiso_logo.gif" />
-</div>
-<h1 style="color:#333;">前道荘 宿泊予約カレンダー</h1>
-<iframe src="https://calendar.google.com/calendar/embed?src=thnr0pmm67hg9g11fcdtibareg%40group.calendar.google.com&ctz=Asia/Tokyo" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+<?php include_once(dirname(__FILE__) . '/../analyticstracking.php') ?>
+<div class="container">
+    <!-- navbar -->
+    <div class="row">
+    <div class="page-header">
+        <h2>
+            <a href="http://meemichisou.web.fc2.com/"><img src="images/memichiso_logo.gif" alt="民宿　前道荘 メーミチソウ" /></a>
+            <small>沖縄、那覇近郊にある素泊まり民宿『前道荘（メーミチソウ）』</small>
+        </h2>
+    </div>
+    </div><!-- /navbar -->
+
+    <!-- content -->
+    <div class="row">
+        <h1 style="padding:6px;border-bottom:1px solid #CC0000;font-size:16px;color:#333;"><span class="glyphicon glyphicon-calendar" aria-hidden="true" style="color:#CC0000;"></span> 前道荘 宿泊予約カレンダー</h1>
+        <div style="margin-top:30px;">
+            <iframe src="https://calendar.google.com/calendar/embed?src=thnr0pmm67hg9g11fcdtibareg%40group.calendar.google.com&ctz=Asia/Tokyo" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+        </div>
+    </div><!-- /content -->
+
+    <!-- footer -->
+    <div class="row" style="margin-top:30px;">
+        <p><a href="http://meemichisou.web.fc2.com/">民宿『前道荘（メーミチソウ）』</a><br />
+        〒900-0034 沖縄県島尻郡南風原町字喜屋武55番地　 TEL / 090-6637-7582（野原）
+        </p>
+        <p style="color:#aaa;">※宴会目的のご宿泊はご遠慮願います。</p>
+        <div id="footer">
+            Copyright(C) <a href="http://meemichisou.web.fc2.com/">前道荘</a> Corporation All rights reserved.</p>
+        </div>
+    </div>
+
+</div><!-- /container-->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
